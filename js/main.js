5abc6dfda535530129167cca51c54cf094705f76
Neron Kevin <neronkevin@gmail.com>
2022-12-05 14:19:14 +0100

    Ajout des objectifs du challenge dans main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert("OK");
+//Objectif du Challenge
+
+/*
+Un bug informatique empêche le père Noël de lire la lettre d'un enfant. On dirait que le texte est à l'envers et que chaque mot aussi 😱 il faut remettre tout ça dans l'ordre !
+*/
+
+const lettre =
+  ".lëoN erèP rehC nom ,étarf esib al ,àlioV .sulp ne sulp ed eriaf ne'd stemorp et ej te sèrgorp sed emêm dnauq siaf ej euq evuort ej iom rac reunitnoc à regaruocne'm ruop 2 enozraW te dleifelttaB ,5SP enu retroppa'm neib emêm dnauq sarduov ut euq te iom ertnoc éhcâf port sap sares en ut euq cnod erèpse'J ! xueim tnemellet tse'c ércér al ,eénruoj al etuot relliavart ed eliciffid uep nu tse'c tnevuos siam repus tse'c elocé'l ,sias ut ,siup te niap ed uaecrom nu ceva reuoj erèférp ej ,epuos am regnam ed ednamed em namaM dnauq ,spmet ne spmet ed ,siam stroffe sed siaf ej ,iom ,elpmexe raP .» non « siof sed siam » iuo « siof sed : elbissop sruojuot sap tse'n ec siam ,elocé'l à neib tnelliavart sli'uq te segas tneios stnafne sel euq tiarduov ednom el tuoT .sulp non elicaf sap tse'n ec ,tnafne nu ertê ,euq tse'c ,» terces « nu erid et siav ej ,lëoN erèP sias ut siam ,» iom ed esuac à uep nu tse'c te diorf sèrt sa ut leic ud sdnecsed ut dnauq « euq te erbmecéD ed siom ua liavart ed puocuaeb sa ut rac elicaf sèrt ertê sap tiod en aç » lëoN erèP « el ertê'uq sias eJ .sna 43 ia'j te etirféD navY elleppa'm eJ ,lëoN erèP rehC";
+
+// Résultat attendu
+// La lettre dans le bon ordre
+
+//TIPS
+// Utiliser la méthode .split() sur la lettre pour créer un tableau de mots
+// Pour chaque mot, vous pouvez utiliser la méthode .split() pour créer un tableau de lettre, puis la méthode .reverse() pour inverser le tableau puis la méthode .join() pour reformer un mot
